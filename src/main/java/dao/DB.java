6efd6a43e39f88815b0f762f9a7ca2821e1f3772
Sharon Kimani <sharonkimani01@gmail.com>
2020-10-04 23:21:32 +0300

    Added logic to DB class file

--- src/main/java/dao/DB.java
+++ src/main/java/dao/DB.java
@@ -6,3 +6,4 @@ public class DB {
 
   //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/organizational_api", "sharon", "12445");
     public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-3-210-255-177.compute-1.amazonaws.com:5432/d1p63pem95jbd9","jlnedeegkpoagr", "bd955e30746b2ad521d91ba8a1e70f2c47768055c4476c2d37a7589d1c601da7");
+}
